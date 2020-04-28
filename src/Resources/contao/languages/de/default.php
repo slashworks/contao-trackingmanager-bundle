de4899267ddff0b6ca20c23401cde0bc6579ef69
stefanmelz <stefan@slash-works.de>
2020-04-28 12:07:49 +0200

    bugfix labels

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -3,8 +3,8 @@
 
 $GLOBALS['TL_LANG']['tl_content']['ce_masonry']['more'] = 'Weiterlesen';
 
-$GLOBALS['TL_LANG']['MOD']['tmSettings'] = 'Cookies';
-$GLOBALS['TL_LANG']['MOD']['tmConfig'] = 'Statistik';
+$GLOBALS['TL_LANG']['MOD']['tmSettings'] = array('Cookies','Cookies hinzufügen');
+$GLOBALS['TL_LANG']['MOD']['tmConfig'] = array('Statistik','Gespeicherte Konfigurationenn anzeigen');
 
 // tl_page
 $GLOBALS['TL_LANG']['tl_page']['tm_basecookie'][0] = 'Label für Basis Cookie';
